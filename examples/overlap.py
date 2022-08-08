fix sign

examples/overlap.py
@@ -1,32 +1,48 @@
 import numpy as np
 
 from time import time
+
+from qiskit import IBMQ, Aer
 from qiskit.circuit import QuantumCircuit, ParameterVector
-from qiskit.circuit.library import EfficientSU2
-from qiskit.opflow import QFI, StateFn
+from qiskit.circuit.library import EfficientSU2, RealAmplitudes
+from qiskit.opflow import QFI, StateFn, PauliExpectation, CircuitSampler
 
 from surfer.qfi import OverlapQFI, ReverseQFI
 
-num_qubits = [2, 4, 8, 16]
-reps = 4
-runs = 5
+num_qubits = 27
+coupling_map = [
+    (0, 1), (4, 7), (10, 12), (15, 18), (21, 23), (24, 25),
+    (22, 19), (16, 14), (11, 8), (5, 3),
+] + [
+    (1, 4), (7, 6), (12, 13), (18, 17), (23, 24), (25, 26),
+    (19, 20), (14, 11), (8, 9), (3, 2)
+] + [
+    (7, 10), (12, 15), (18, 21), (25, 22), (19, 16),
+    (13, 14), (8, 5), (2, 1)
+]
+reps = 1
 
+# circuit = EfficientSU2(num_qubits, reps=reps, entanglement=coupling_map).decompose()
+circuit = RealAmplitudes(num_qubits, reps=reps, entanglement=coupling_map).decompose()
 
-def run_single(num_qubits, reps):
-    circuit = EfficientSU2(num_qubits, reps=reps, entanglement="pairwise")
-    values = np.zeros(circuit.num_parameters)
-    start = time()
-    qfi = OverlapQFI(clifford=True).compute(circuit, values)
-    return time() - start
+parameters = circuit.parameters
+values = np.zeros(circuit.num_parameters)
 
+for i in range(circuit.num_qubits):
+    values[~i] = np.pi / 2
 
-times = []
-times_std = []
-for n in num_qubits:
-    results = [run_single(n, reps) for _ in range(runs)]
-    times.append(np.mean(results))
-    times_std.append(np.std(results))
+# start = time()
+# qfi = ReverseQFI(do_checks=False).compute(circuit, values)
+# time_taken = time() - start
 
-    print(f"{n} qubits took {times[-1]}s +- {times_std[-1]}")
+# print(time_taken)
+# print(qfi)
 
-np.save("cliffsimv2_su2r4.npy", np.vstack((num_qubits, times, times_std)))
+start = time()
+qfi = OverlapQFI(clifford=True).compute(circuit, values)
+time_taken = time() - start
+
+print(time_taken)
+print(qfi)
+np.save("qfi_cliff_realamp_plus_kolkata.npy", qfi)
+np.save("qfi_cliff_realamp_+_kolkata.npy", qfi)

surfer/qfi/overlap_qfi.py
@@ -61,13 +61,15 @@ class OverlapQFI(QFICalculator):
         qfi = np.zeros((len(parameters), len(parameters)), dtype=complex)
 
         for i, p_i in enumerate(parameters):
+            print(f"Row {i}/{len(parameters)}")
             # TODO maybe set to 1 directly if possible?
             qfi[i, i] += self.compute_curvature(derivatives, p_i, p_i)
-            qfi[i, i] += self.compute_phasefix(derivatives, p_i, p_i, data)
+            qfi[i, i] -= self.compute_phasefix(derivatives, p_i, p_i, data)
             for j_, p_j in enumerate(parameters[i + 1 :]):
+                print(f"Col {j_}/{len(parameters[i + 1:])}")
                 j = i + 1 + j_
                 qfi[i, j] += self.compute_curvature(derivatives, p_i, p_j)
-                qfi[i, j] += self.compute_phasefix(derivatives, p_i, p_j, data)
+                qfi[i, j] -= self.compute_phasefix(derivatives, p_i, p_j, data)
 
         qfi += np.triu(qfi, k=1).T
 
@@ -79,7 +81,8 @@ class OverlapQFI(QFICalculator):
         data = data_j.compose(inverse(data_i))
         # circuit = circuit_j.compose(circuit_i.inverse())
         # bound = circuit.bind_parameters(values)
-        return np.conj(coeff_i) * coeff_j * self.execute(data)
+        ret = np.conj(coeff_i) * coeff_j * self.execute(data)
+        return ret
 
     def compute_phasefix(self, derivatives, p_i, p_j, data):
         coeff_i, data_i = derivatives[p_i][0]
@@ -88,7 +91,8 @@ class OverlapQFI(QFICalculator):
         left = data.compose(inverse(data_i))
         right = data_j.compose(inverse(data))
 
-        return np.conj(coeff_i) * coeff_j * self.execute(left) * self.execute(right)
+        ret = np.conj(coeff_i) * coeff_j * self.execute(left) * self.execute(right)
+        return ret
 
     def execute(self, data):
         if isinstance(data, Clifford):
@@ -204,8 +208,8 @@ class Cliffordize(TransformationPass):
 
             if is_identity(node.op):
                 dag.remove_op_node(node)
-
-            try_replace_ry(dag, node)
+            else:
+                try_replace_ry(dag, node)
 
         return dag
 